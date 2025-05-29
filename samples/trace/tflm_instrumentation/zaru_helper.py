ccf235a8f0f5c1e1131346072cb7e9c04e44d7cf
Maciej Sobkowski <msobkowski@antmicro.com>
2025-05-29 09:09:53 +0200

    [#77497] samples: add TFLM instrumentation sample
    This sample is based on the  TFLM hello world sample, modified to
    support Zephyr's instrumentation subsystem.
    
    Signed-off-by: Maciej Sobkowski <msobkowski@antmicro.com>

---
+++ samples/trace/tflm_instrumentation/zaru_helper.py
@@ -0,0 +1,69 @@
+import subprocess
+
+
+def check_instrumentation_enabled_status(instrumentation_script, pty_link):
+    proc = subprocess.Popen(
+        [instrumentation_script, "--serial", pty_link, "status"], stdout=subprocess.PIPE
+    )
+    (output, _) = proc.communicate()
+    exit_code = proc.wait()
+    if exit_code != 0:
+        raise Exception("Failed to get instrumentation status: {}.".format(output))
+    if "Trace supported" not in output.decode("utf-8"):
+        raise Exception("Instrumentation callgraph mode is not enabled")
+
+
+def set_instrumentation_coupled_trigger(
+    instrumentation_script, pty_link, trigger_function
+):
+    proc = subprocess.Popen(
+        [instrumentation_script, "--serial", pty_link, "trace", "-c", trigger_function],
+        stdout=subprocess.PIPE,
+    )
+    (output, _) = proc.communicate()
+    exit_code = proc.wait()
+    if exit_code != 0:
+        raise Exception("Failed to set coupled trigger: {}.".format(output))
+
+
+def trigger_instrumentation_reboot(instrumentation_script, pty_link):
+    proc = subprocess.Popen(
+        [instrumentation_script, "--serial", pty_link, "reboot"], stdout=subprocess.PIPE
+    )
+    (output, _) = proc.communicate()
+    exit_code = proc.wait()
+    if exit_code != 0:
+        raise Exception("Failed to reboot target: {}.".format(output))
+
+
+def generate_instrumentation_callgraph(instrumentation_script, pty_link):
+    proc = subprocess.Popen(
+        [instrumentation_script, "--serial", pty_link, "trace"], stdout=subprocess.PIPE
+    )
+    (output, _) = proc.communicate()
+    exit_code = proc.wait()
+    if exit_code != 0:
+        raise Exception("Failed to reboot target: {}.".format(output))
+    with open("callgraph.txt", "wb") as f:
+        f.write(output)
+
+
+def generate_instrumentation_perfetto_trace(instrumentation_script, pty_link):
+    proc = subprocess.Popen(
+        [
+            instrumentation_script,
+            "--serial",
+            pty_link,
+            "trace",
+            "--perfetto",
+            "--output",
+            "zephyr_trace.json",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    (output, _) = proc.communicate()
+    exit_code = proc.wait()
+    if exit_code != 0:
+        raise Exception("Failed to reboot target: {}.".format(output))
+    with open("callgraph.txt", "wb") as f:
+        f.write(output)
