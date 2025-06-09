ae1ad61bba13592d7f8bcb91d76d7fe15c0c289c
Filip Kokosinski <fkokosinski@antmicro.com>
2025-06-09 12:18:46 +0200

    [#78627] zpl: integrate USB backend

--- zpl/lib.c
+++ zpl/lib.c
@@ -1,5 +1,6 @@
 #include <zephyr/tracing/tracing.h>
 #include <zephyr/tracing/tracing_format.h>
+#include <zephyr/usb/usb_device.h>
 
 #ifdef CONFIG_TRACING_TEST
 void zpl_named_event(const char *name, uint32_t arg0, uint32_t arg1)
@@ -10,5 +11,16 @@ void zpl_named_event(const char *name, uint32_t arg0, uint32_t arg1)
 
 int zpl_init(void)
 {
+
+#if defined(CONFIG_USB_DEVICE_STACK)
+	int ret;
+
+	ret = usb_enable(NULL);
+	if (ret) {
+		printk("usb backend enable failed");
+		return 1;
+	}
+#endif /* CONFIG_USB_DEVICE_STACK */
+
 	return 0;
 }
