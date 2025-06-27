acbe8f7252077beb67b1d4525b4d9a3e2b9815ec
Maciej Torhan <mtorhan@antmicro.com>
2025-06-27 14:37:19 +0200

    [#79214] zpl: Add CPU load profiling
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ include/zpl/cpu_load.h
@@ -0,0 +1,29 @@
+#ifndef ZPL_CPU_LOAD_H_
+#define ZPL_CPU_LOAD_H_
+
+/**
+ * CPU load profiling.
+ */
+
+#include <stdint.h>
+
+#if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
+/* CPU load event ID */
+#define ZPL_CPU_LOAD_EVENT 0xC0
+
+/**
+ * CPU load event structure.
+ */
+typedef struct __packed {
+	uint32_t timestamp;
+	uint8_t id;
+	uint16_t cpu_load; /* CPU load denoted by a number 0-1000 */
+} zpl_cpu_load_event_t;
+#endif /* defined(CONFIG_ZPL_TRACE_FORMAT_CTF) */
+
+/**
+ * Emits CPU load event.
+ */
+void zpl_emit_cpu_load_event(void);
+
+#endif /* ZPL_CPU_LOAD_H_ */
---
+++ zpl/cpu_load.c
@@ -0,0 +1,41 @@
+#include <zpl/cpu_load.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/debug/cpu_load.h>
+#include <zephyr/tracing/tracing_format.h>
+
+void zpl_emit_cpu_load_event(void)
+{
+	int cpu_load = cpu_load_get(true);
+
+#if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
+	zpl_cpu_load_event_t zpl_cpu_load_event = {
+		.timestamp = k_cyc_to_ns_floor64(k_cycle_get_32()),
+		.id = ZPL_CPU_LOAD_EVENT,
+		.cpu_load = cpu_load,
+	};
+	tracing_format_raw_data(
+		(uint8_t *)&zpl_cpu_load_event, sizeof(zpl_cpu_load_event_t)
+	);
+#elif defined(CONFIG_ZPL_TRACE_FORMAT_PLAINTEXT)
+	TRACING_STRING("zpl_cpu_load_event: cpu_load=%d.%d%\n", cpu_load / 10, cpu_load % 10);
+#endif /* CONFIG_ZPL_TRACE_FORMAT_* */
+}
+
+#ifdef CONFIG_ZPL_CPU_LOAD_TRACE
+
+static void zpl_profile_cpu_load(void)
+{
+	while (true) {
+		zpl_emit_cpu_load_event();
+		k_msleep(CONFIG_ZPL_CPU_LOAD_PROFILING_INTERVAL);
+	}
+}
+
+K_THREAD_DEFINE(zpl_cpu_load_profiling,
+				CONFIG_ZPL_CPU_LOAD_PROFILING_THREAD_STACK_SIZE,
+				zpl_profile_cpu_load, NULL, NULL, NULL,
+				CONFIG_ZPL_CPU_LOAD_PROFILING_THREAD_PRIORITY,
+				0, CONFIG_ZPL_CPU_LOAD_PROFILING_THREAD_DELAY);
+
+#endif /* CONFIG_ZPL_CPU_LOAD_TRACE */
