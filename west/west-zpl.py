b85484a4e34b4f242f3f5a3b131f6af3e3cd6205
Filip Kokosinski <fkokosinski@antmicro.com>
2025-06-30 15:18:11 +0200

    [#79500] west: add zpl-uart-capture command

--- west/west-zpl.py
+++ west/west-zpl.py
@@ -3,6 +3,7 @@
 import subprocess
 from textwrap import dedent
 
+import serial
 from west.commands import WestCommand
 
 
@@ -56,3 +57,47 @@ class ZplGdbCapture(WestCommand):
         if exit_code != 0:
             self.err(output)
             self.die("Failed to capture tracing data!")
+
+
+class ZplUartCapture(WestCommand):
+    """Main class for the zpl-uart-capture command."""
+
+    def __init__(self):
+        """Init function for the zpl-uart-capture command."""
+        super().__init__(
+            "zpl-uart-capture",
+            "Capture traces using UART",
+            dedent("""
+                Capture traces using UART.
+
+                This command capures traces using the serial interface."""),
+        )
+
+    def do_add_parser(self, parser_adder):
+        parser = parser_adder.add_parser(self.name, help=self.help, description=self.description)
+
+        parser.add_argument("serial_port", help="Seral port")
+        parser.add_argument("serial_baudrate", help="Seral baudrate")
+        parser.add_argument("output_path", help="Capture output path")
+
+        return parser
+
+    def do_run(self, args, unknown_args):
+        ser = serial.Serial(args.serial_port, args.serial_baudrate)
+        if ser.is_open:
+            self.inf(f"Capturing traces on {ser.port}@{ser.baudrate}...")
+            self.inf("Press C-c to stop.")
+        else:
+            self.die(f"Couldn't open port {ser.port}!")
+
+        ser.write("enable\r".encode())
+
+        with open(args.output_path, "wb") as f:
+            while True:
+                try:
+                    while ser.inWaiting() > 0:
+                        f.write(ser.read())
+                except KeyboardInterrupt:
+                    break
+
+        ser.close()
