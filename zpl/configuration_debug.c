c4a346de368ddbb1cf462102b88974595a1a8d55
Krzysztof Sychla <ksychla@antmicro.com>
2025-07-07 15:57:03 +0200

    [#80012] zpl: Add debug runtime configuration

---
+++ zpl/configuration_debug.c
@@ -0,0 +1,31 @@
+#include <zpl/configuration.h>
+#include <zephyr/kernel.h>
+
+#define ZPL_WAIT_FOR_CONF_DEF(name, config)                 \
+	void zpl_wait_for_##name(void)                            \
+	{                                                         \
+		while (!IS_ENABLED(config) && !debug_configs.name) {    \
+			k_msleep(100);                                        \
+		}                                                       \
+	}
+
+#define ZPL_CHECK_IF_CONF_DEF(name, config)                 \
+	bool zpl_check_if_conf_##name(void)                       \
+	{                                                         \
+		return IS_ENABLED(config) || debug_configs.name;        \
+	}
+
+#define ZPL_CONF_DEBUG_STRUCT_FIELD_DEF(name) bool name
+#define ZPL_CONF_DEBUG_DEF(name, config)   \
+	ZPL_WAIT_FOR_CONF_DEF(name, config)      \
+	ZPL_CHECK_IF_CONF_DEF(name, config)
+
+struct runtime_configs {
+	/* Struct fields definitions */
+	ZPL_CONF_DEBUG_STRUCT_FIELD_DEF(mem_usage_trace);
+};
+
+static struct runtime_configs volatile debug_configs;
+
+/* Config definitions */
+ZPL_CONF_DEBUG_DEF(mem_usage_trace, CONFIG_ZPL_MEMORY_USAGE_TRACE)
