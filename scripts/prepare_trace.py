0461061ec90bda6dc616e9a0948c7a1c307436b9
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-07-07 13:10:26 +0200

    [#79243] scripts: prepare_trace: Get symbols for memory regions from zephyr.elf
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- scripts/prepare_trace.py
+++ scripts/prepare_trace.py
@@ -9,7 +9,9 @@ Script preparing trace (in TEF) based on the CTF trace and metadata, e.g. TFLM m
 import argparse
 import json
 import os
+import subprocess
 import sys
+from collections import defaultdict
 from pathlib import Path
 
 from ctf2tef import (
@@ -56,6 +58,10 @@ CUSTOM_EVENTS = [
     ),
 ]
 
+# Set with initial addresses for memory regions,
+# used for mapping them to their symbols from built Zephyr ELF
+MEMORY_ADDRESSES = set()
+
 
 def memory_data(msg) -> dict:
     """
@@ -63,6 +69,8 @@ def memory_data(msg) -> dict:
     """
     if not (args := msg.event.payload_field):
         return {}
+    if "memory_addr" in args:
+        MEMORY_ADDRESSES.add(int(args["memory_addr"]))
     try:
         # Get enum label and remove ZPL_ prefix
         # This will override "memory_region"
@@ -75,6 +83,7 @@ def memory_data(msg) -> dict:
 # and value is a definition of new metadata event.
 CUSTOM_METADATA = {
     "zpl_memory": CustomMetadataDefinition("MEMORY", None, memory_data),
+    "thread_info": CustomMetadataDefinition("THREAD", None, None),
 }
 
 
@@ -94,6 +103,49 @@ def add_model_metadata(trace: list, data: dict):
             "args": data,
         },
     )
+
+
+def extract_memory_symbols(zephyr_elf_path: Path):
+    """
+    Extracts memory symbols from the provided Zephyr ELF.
+
+    It uses `nm` from GNU binutils to get all available symbols
+    and filteres out ones that have not appeared in trace.
+    """
+    if not zephyr_elf_path.exists():
+        print(
+            f"Zephyr ELF ({zephyr_elf_path}) does not exist, memory symbols will not be extracted",
+            file=sys.stderr,
+        )
+        return
+    if 0 != subprocess.call(["which", "nm"], stdout=subprocess.DEVNULL):
+        print(
+            "`nm` is not available, please install binutils to extract symbols of memory regions",
+            file=sys.stderr,
+        )
+        return
+
+    nm = subprocess.run(["nm", str(zephyr_elf_path.absolute())], stdout=subprocess.PIPE)
+    if 0 != nm.returncode:
+        print("Symbol extraction failed", file=sys.stderr)
+        return
+
+    nm_output = nm.stdout.decode()
+    addr_to_symbol = defaultdict(list)
+    for line in nm_output.splitlines():
+        addr, _, name = line.split(" ")
+        addr_to_symbol[addr.lower()].append(name)
+
+    mem_symbols = {}
+    for addr in MEMORY_ADDRESSES:
+        addr_hex = f"{addr:x}"
+        if addr_hex not in addr_to_symbol or not addr_to_symbol[addr_hex]:
+            print(f"Cannot find symbol for address 0x{addr_hex}", file=sys.stderr)
+            continue
+        # Choose last found symbol
+        mem_symbols[addr] = addr_to_symbol[addr_hex][-1]
+
+    return mem_symbols
 
 
 if __name__ == "__main__":
@@ -135,6 +187,12 @@ if __name__ == "__main__":
         "to the final trace as a metadata",
         default=None,
     )
+    parser.add_argument(
+        "--zephyr-elf-path",
+        type=Path,
+        help="Path to the built Zephyr ELF, required for extracting symbols of memory regions",
+        default=None,
+    )
     args = parser.parse_args(sys.argv[1:])
 
     # Convert CTF
@@ -155,15 +213,36 @@ if __name__ == "__main__":
             for t_name, tid in thread_name.items()
         ]
 
+    # Metadata about TFLM model
     if args.tflm_model_path is not None:
         from extract_tflite_model_data import extract_model_data
 
         add_model_metadata(tef_trace, extract_model_data(args.tflm_model_path))
+
+    # Metadata about TVM model
     if args.tvm_model_path is not None:
         from extract_tvm_model_data import extract_model_data
 
         add_model_metadata(tef_trace, extract_model_data(args.tvm_model_path))
 
+    # Metadata with memory symbols
+    if MEMORY_ADDRESSES:
+        if args.zephyr_elf_path is None:
+            args.zephyr_elf_path = Path(".") / "build" / "zephyr" / "zephyr.elf"
+        mem_symbols = extract_memory_symbols(args.zephyr_elf_path)
+        tef_trace.append(
+            {
+                "name": "MEMORY::SYMBOLS",
+                "cat": "zephyr",
+                "ph": EventPhase.METADATA.value,
+                "pid": 0,
+                "tid": 0,
+                "ts": 0,
+                "args": mem_symbols,
+            },
+        )
+
+    # Print or save the result
     if args.output is not None:
         args.output.parent.mkdir(parents=True, exist_ok=True)
         with args.output.open("w") as fd:
