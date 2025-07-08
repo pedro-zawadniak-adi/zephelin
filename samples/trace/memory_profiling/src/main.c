c6e5e629c4d4c027f71a8eb238437701af796fcb
Krzysztof Sychla <ksychla@antmicro.com>
2025-07-08 09:15:58 +0200

    [#80056] samples: memory_profiling: Add z_malloc_heap test
    Signed-off-by: Krzysztof Sychla <ksychla@antmicro.com>

--- samples/trace/memory_profiling/src/main.c
+++ samples/trace/memory_profiling/src/main.c
@@ -1,5 +1,6 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/kernel.h>
+#include <stdlib.h>
 #include <zpl/lib.h>
 
 K_MEM_SLAB_DEFINE(test_mem_slab, 256, 32, 4);
@@ -13,12 +14,15 @@ void recursive_function(int counter)
 	}
 	int *k_heap_variable = k_heap_alloc(&test_k_heap, 7, K_MSEC(100));
 	int *heap_variable = k_malloc(sizeof(int) * 5);
+	int *stdlib_malloc_test = malloc(sizeof(int) * 512);
+	stdlib_malloc_test[0] = 5;
 
 	printk("Counter: %d\n", counter);
 	if (counter > 0) {
 		k_msleep(200);
 		recursive_function(counter - 1);
 	}
+	free(stdlib_malloc_test);
 	k_free(heap_variable);
 	k_heap_free(&test_k_heap, k_heap_variable);
 	k_mem_slab_free(&test_mem_slab, block_ptr);
