ad4dbb456c79b3f20c3684bb80709238ef97f922
Maciej Torhan <mtorhan@antmicro.com>
2025-07-11 08:43:31 +0200

    [#80017] zpl: Add inference event
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ include/zpl/inference_event.h
@@ -0,0 +1,35 @@
+#ifndef ZPL_INFERENCE_EVENT_H_
+#define ZPL_INFERENCE_EVENT_H_
+
+/**
+ * Inference duration profiling.
+ */
+
+#include <zephyr/kernel.h>
+#include <stdint.h>
+
+#if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
+/* Inference events IDs */
+#define ZPL_INFERENCE_ENTER_EVENT 0xD0
+#define ZPL_INFERENCE_EXIT_EVENT 0xD1
+
+/**
+ * Inference event structure.
+ */
+typedef struct __packed {
+	uint32_t timestamp;
+	uint8_t id;
+} zpl_inference_event_t;
+#endif /* defined(CONFIG_ZPL_TRACE_FORMAT_CTF) */
+
+/**
+ * Marks inference enter.
+ */
+void zpl_inference_enter();
+
+/**
+ * Marks inference exit.
+ */
+void zpl_inference_exit();
+
+#endif /* ZPL_INFERENCE_EVENT_H_ */
---
+++ zpl/inference_event.c
@@ -0,0 +1,36 @@
+#include "zpl/inference_event.h"
+
+#include <zephyr/kernel.h>
+#include <zephyr/tracing/tracing_format.h>
+
+void zpl_inference_enter()
+{
+#if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
+	zpl_inference_event_t zpl_inference_enter_event = {
+		.timestamp = k_cyc_to_ns_floor64(k_cycle_get_32()),
+		.id = ZPL_INFERENCE_ENTER_EVENT,
+	};
+
+	tracing_format_raw_data(
+		(uint8_t *)&zpl_inference_enter_event, sizeof(zpl_inference_event_t)
+	);
+#elif defined(CONFIG_ZPL_TRACE_FORMAT_PLAINTEXT)
+	TRACING_STRING("zpl_inference_enter:\n");
+#endif /* CONFIG_ZPL_TRACE_FORMAT_* */
+}
+
+void zpl_inference_exit()
+{
+#if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
+	zpl_inference_event_t zpl_inference_exit_event = {
+		.timestamp = k_cyc_to_ns_floor64(k_cycle_get_32()),
+		.id = ZPL_INFERENCE_EXIT_EVENT,
+	};
+
+	tracing_format_raw_data(
+		(uint8_t *)&zpl_inference_exit_event, sizeof(zpl_inference_event_t)
+	);
+#elif defined(CONFIG_ZPL_TRACE_FORMAT_PLAINTEXT)
+	TRACING_STRING("zpl_inference_exit:\n");
+#endif /* CONFIG_ZPL_TRACE_FORMAT_* */
+}
