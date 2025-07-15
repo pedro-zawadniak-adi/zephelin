949b464788c07728d469a9cbe53919b85fc036df
Filip Kokosinski <fkokosinski@antmicro.com>
2025-07-15 13:42:23 +0200

    [#80421] samples/trace/named_event: add more events and delay

--- samples/trace/named_event/src/main.c
+++ samples/trace/named_event/src/main.c
@@ -1,11 +1,13 @@
+#include <zephyr/kernel.h>
 #include <zpl/lib.h>
 
 int main(void)
 {
 	zpl_init();
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 100; i++) {
 		sys_trace_named_event("counter_value", i, 0);
+		k_sleep(K_SECONDS(1));
 	}
 
 	return 0;
