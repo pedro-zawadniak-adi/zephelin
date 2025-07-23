5d9f6a00713547edf9791cba4b40336a57c3fb78
Maciej Torhan <mtorhan@antmicro.com>
2025-07-23 12:16:05 +0200

    [#80265] samples: trace: Move boards overlays to common
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ samples/trace/common/boards.cmake
@@ -0,0 +1,13 @@
+string(REPLACE "/" "_" board_sanitized "${BOARD}")
+
+string(REPLACE "/" ";" board_list "${BOARD}")
+list(GET board_list 0 board_short)
+
+set(board_overlay "${CMAKE_CURRENT_LIST_DIR}/boards/${board_sanitized}.overlay")
+set(board_short_overlay "${CMAKE_CURRENT_LIST_DIR}/boards/${board_short}.overlay")
+
+if(EXISTS "${board_overlay}")
+  set(EXTRA_DTC_OVERLAY_FILE "${board_overlay}" CACHE STRING "Auto-loaded board overlay")
+elseif(EXISTS "${board_short_overlay}")
+  set(EXTRA_DTC_OVERLAY_FILE "${board_short_overlay}" CACHE STRING "Auto-loaded board overlay")
+endif()
