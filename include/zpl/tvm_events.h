70240f92833db119f2aa45a482a2d804979acd3c
Krzysztof Sychla <ksychla@antmicro.com>
2025-08-06 12:52:14 +0200

    [#81557] include: tvm_event: Remove duplicated code

--- include/zpl/tvm_events.h
+++ include/zpl/tvm_events.h
@@ -16,16 +16,7 @@ typedef struct __packed {
 	uint8_t op_idx;
 	uint16_t tag_len;
 	uint8_t tag[CONFIG_ZPL_TRACE_CTF_MAX_LONG_STR_LEN];
-} zpl_tvm_enter_event_t;
-
-typedef struct __packed {
-	uint32_t timestamp;
-	uint8_t id;
-	uint32_t thread_id;
-	uint8_t op_idx;
-	uint16_t tag_len;
-	uint8_t tag[CONFIG_ZPL_TRACE_CTF_MAX_LONG_STR_LEN];
-} zpl_tvm_exit_event_t;
+} zpl_tvm_event_t;
 #endif /* defined(CONFIG_ZPL_TRACE_FORMAT_CTF) */
 
 void zpl_emit_tvm_enter_event(uint32_t cycles, uint8_t op_idx, const char* tag);
--- zpl/tvm_events.c
+++ zpl/tvm_events.c
@@ -12,13 +12,13 @@
 
 #include <stdio.h>
 
-void zpl_emit_tvm_enter_event(uint32_t cycles, uint8_t op_idx, const char* tag)
+void __zpl_emit_tvm_event(uint32_t cycles, uint8_t op_idx, const char* tag, bool is_exit)
 {
 #if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
-	zpl_tvm_enter_event_t zpl_tvm_enter_event = {
+	zpl_tvm_event_t zpl_tvm_enter_event = {
 		.timestamp = k_cyc_to_ns_floor64(cycles),
 		.op_idx = op_idx,
-		.id = ZPL_TVM_BEGIN_EVENT,
+		.id = is_exit ? ZPL_TVM_END_EVENT : ZPL_TVM_BEGIN_EVENT,
 		.thread_id = (uint32_t)k_current_get(),
 		.tag_len = CONFIG_ZPL_TRACE_CTF_MAX_LONG_STR_LEN,
 	};
@@ -28,26 +28,16 @@ void zpl_emit_tvm_enter_event(uint32_t cycles, uint8_t op_idx, const char* tag)
 		(uint8_t *)&zpl_tvm_enter_event, sizeof(zpl_tvm_enter_event)
 	);
 #elif defined(CONFIG_ZPL_TRACE_FORMAT_PLAINTEXT)
-	TRACING_STRING("zpl_tvm_enter_event: op_idx=%d tag=%s\n", op_idx, tag);
+	TRACING_STRING("zpl_tvm_%s_event: op_idx=%d tag=%s\n", is_exit ? "exit" : "enter", op_idx, tag);
 #endif /* CONFIG_ZPL_TRACE_FORMAT_* */
+}
+
+void zpl_emit_tvm_enter_event(uint32_t cycles, uint8_t op_idx, const char* tag)
+{
+	__zpl_emit_tvm_event(cycles, op_idx, tag, false);
 }
 
 void zpl_emit_tvm_exit_event(uint32_t cycles, uint8_t op_idx, const char* tag)
 {
-#if defined(CONFIG_ZPL_TRACE_FORMAT_CTF)
-	zpl_tvm_exit_event_t zpl_tvm_exit_event = {
-		.timestamp = k_cyc_to_ns_floor64(cycles),
-		.op_idx = op_idx,
-		.id = ZPL_TVM_END_EVENT,
-		.thread_id = (uint32_t)k_current_get(),
-		.tag_len = CONFIG_ZPL_TRACE_CTF_MAX_LONG_STR_LEN,
-	};
-	snprintf(&(zpl_tvm_exit_event.tag[0]), CONFIG_ZPL_TRACE_CTF_MAX_LONG_STR_LEN, "%s", tag);
-
-	tracing_format_raw_data(
-		(uint8_t *)&zpl_tvm_exit_event, sizeof(zpl_tvm_exit_event)
-	);
-#elif defined(CONFIG_ZPL_TRACE_FORMAT_PLAINTEXT)
-	TRACING_STRING("zpl_tvm_exit_event: op_idx=%d tag=%s\n", op_idx, tag);
-#endif /* CONFIG_ZPL_TRACE_FORMAT_* */
+	__zpl_emit_tvm_event(cycles, op_idx, tag, true);
 }
