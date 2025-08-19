2daccab02cd80179b24eb8b6aceaf2816e29a571
Andrii Chmutov <achmutov@antmicro.com>
2025-08-19 17:14:27 +0200

    [#82040] scripts: install_deps: Install flatc
    Install flatc from GitHub in order to support new syntax, because
    old flatbuffers-compiler/noble 2.0.8+dfsg1-6build1, which is used by
    zephyrprojectrtos/ci:v0.28.2, raises an error
    
    Signed-off-by: Andrii Chmutov <achmutov@antmicro.com>

--- scripts/ci/install_deps.sh
+++ scripts/ci/install_deps.sh
@@ -19,6 +19,10 @@ apt install -yqq xxd gdb-multiarch mono-complete
 # install deps with pip
 pip install -r requirements.txt
 
+# install recent flatbuffers-compiler
+wget https://github.com/google/flatbuffers/releases/download/v25.2.10/Linux.flatc.binary.g++-13.zip -O flatc.zip
+unzip flatc.zip -d /usr/bin && rm flatc.zip
+
 # install Renode
 wget -q "https://dl.antmicro.com/projects/renode/builds/${RENODE_VERSION}" -O renode.deb
 dpkg -i renode.deb
