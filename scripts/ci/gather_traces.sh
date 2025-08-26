769c4701b465855cbf554668d1e645190102acde
Maciej Torhan <mtorhan@antmicro.com>
2025-08-26 14:30:03 +0200

    [#82384] scripts: ci: gather_traces: Update script
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- scripts/ci/gather_traces.sh
+++ scripts/ci/gather_traces.sh
@@ -14,29 +14,29 @@ CTF_CONFS=${CTF_CONFS:-"-DCONFIG_ZPL_TRACE_FORMAT_CTF=y
 
 ### TFLM profiler
 west build -p -b $BOARD samples/profiling/tflm_profiler -- ${CTF_CONFS}
-timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py
+timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py --trace-output ./channel0_0
 west zpl-prepare-trace ./channel0_0 \
   --tflm-model-path ./samples/common/tflm/model/magic-wand.tflite \
   -o ./tef_tflm_profiler.json
 
 ### Memory profiling
 west build -p -b $BOARD samples/profiling/memory_profiling -- ${CTF_CONFS}
-timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py
+timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py --trace-output ./channel0_0
 west zpl-prepare-trace ./channel0_0 -o ./tef_memory_profiling.json
 
 ### TVM profiler
 west build -p -b $BOARD samples/profiling/tvm_profiler -- ${CTF_CONFS}
-timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py
+timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py --trace-output ./channel0_0
 west zpl-prepare-trace ./channel0_0 \
   --tvm-model-path ./samples/common/tvm/model/magic-wand-graph.json \
   -o ./tef_tvm_profiler.json
 
 ### Marking code scopes
 west build -p -b $BOARD samples/basic/marking_code_scopes -- ${CTF_CONFS}
-timeout --preserve-status -s INT 30s python3 ./scripts/run_renode.py
+timeout --preserve-status -s INT 30s python3 ./scripts/run_renode.py --trace-output ./channel0_0
 west zpl-prepare-trace ./channel0_0 -o ./tef_marking_code_scopes.json
 
 ### CPU load
 west build -p -b $BOARD samples/profiling/cpu_load -- ${CTF_CONFS}
-timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py
+timeout --preserve-status -s INT 20s python3 ./scripts/run_renode.py --trace-output ./channel0_0
 west zpl-prepare-trace ./channel0_0 -o ./tef_cpu_load.json
